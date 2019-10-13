c6d23d79d851350136cbbdd6763e69fd3a3e126a
Chuntong Gao <heihegao@gmail.com>
2019-10-12 19:12:25 -0700

    display all vehicles and customers - works

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Admin, Resource } from 'react-admin';
-import { UserList } from './users';
+import { Admin, Resource, ListGuesser } from 'react-admin';
+// import { UserList } from './users';
 import jsonServerProvider from 'ra-data-json-server';
 
 const dataProvider = jsonServerProvider(
-    'http://jsonplaceholder.typicode.com'
+    'http://localhost:3300'
 );
 const App = () => (
     <Admin dataProvider={dataProvider}>
-        <Resource name="users" list={UserList} />
+        <Resource name="customers" list={ListGuesser} />
+        <Resource name="vehicles" list={ListGuesser} />
     </Admin>
 );
 
