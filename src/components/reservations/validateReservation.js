4b24346634fb5cb8dea2fbc7aeb746bdac96b61d
Chuntong Gao <heihegao@gmail.com>
2019-10-22 20:14:22 -0700

    add advanced date checking to validate reservations

--- src/components/reservations/validateReservation.js
+++ src/components/reservations/validateReservation.js
@@ -21,6 +21,14 @@ const validateReservation = values => {
             'The end date must be greater or equal than the start date'
         ];
 
+    if (moment(values.fromDate).isBefore(moment()))
+        errors.fromDate = [ 'Start date cannot be in the past' ];
+
+    if (moment(values.fromDate).isAfter('2020-12-01'))
+        errors.fromDate = [ 'Start date cannot be after 2020-12-01' ];
+
+    if (moment(values.toDate).isAfter('2020-12-01'))
+        errors.toDate = [ 'End date cannot be after 2020-12-01' ];
     return errors;
 };
 
