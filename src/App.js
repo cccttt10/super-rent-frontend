6036fc93a069404b26bfb4b95970d18f5a54f5cc
Chuntong Gao <heihegao@gmail.com>
2019-10-24 13:27:52 -0700

    create resources/ dir in src/components/

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import { Admin, Resource } from 'react-admin';
 import Dashboard from './components/dashboard/index';
 import jsonServerProvider from 'ra-data-json-server';
 
-import customers from './components/customers/';
-import vehicles from './components/vehicles/';
-import reservations from './components/reservations/';
-import rents from './components/rents/';
-import returns from './components/returns/';
+import customers from './components/resources/customers/';
+import vehicles from './components/resources/vehicles/';
+import reservations from './components/resources/reservations/';
+import rents from './components/resources/rents/';
+import returns from './components/resources/returns/';
 
 const dataProvider = jsonServerProvider('http://localhost:3300');
 const App = () => (
