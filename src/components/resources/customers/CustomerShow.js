1b05f00902db37862ef1de73be5eb0baeafaf21d
Chuntong Gao <heihegao@gmail.com>
2019-10-26 16:07:08 -0700

    implement CustomerShow

---
+++ src/components/resources/customers/CustomerShow.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Show, TextField, SimpleShowLayout } from 'react-admin';
+import CardActions from '@material-ui/core/CardActions';
+import { DeleteButton, EditButton } from 'react-admin';
+
+const CustomerShow = props => (
+    <Show actions={<CustomerShowActions />} {...props}>
+        <SimpleShowLayout>
+            <TextField
+                source="driversLicence"
+                label="Driver's Licence"
+            />
+            <TextField source="phone" />
+            <TextField source="name" />
+        </SimpleShowLayout>
+    </Show>
+);
+
+const cardActionStyle = {
+    zIndex: 2,
+    display: 'inline-block',
+    float: 'right'
+};
+
+const CustomerShowActions = ({ basePath, data, resource }) => (
+    <CardActions style={cardActionStyle}>
+        <EditButton
+            basePath={basePath}
+            record={data}
+            resource="customers"
+        />
+        <DeleteButton
+            basePath={basePath}
+            record={data}
+            resource="customers"
+        />
+    </CardActions>
+);
+
+export default CustomerShow;
