331250bdfabfda3a7226989a19ad0676732d754f
Chuntong Gao <heihegao@gmail.com>
2019-10-26 22:51:07 -0700

    Updates

---
+++ precache-manifest.324c56d42356996318cf54d5c3d635d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e968dd777c5a8e7621b229e6f3a5615e",
+    "url": "/super-rent-frontend/index.html"
+  },
+  {
+    "revision": "c5f1e501ecd8c225c4d8",
+    "url": "/super-rent-frontend/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e4eff776404590b72887",
+    "url": "/super-rent-frontend/static/js/2.14640b39.chunk.js"
+  },
+  {
+    "revision": "c5f1e501ecd8c225c4d8",
+    "url": "/super-rent-frontend/static/js/main.162bfba3.chunk.js"
+  },
+  {
+    "revision": "2ed220c438b1bfbb1555",
+    "url": "/super-rent-frontend/static/js/runtime-main.69122db4.js"
+  }
+]);
